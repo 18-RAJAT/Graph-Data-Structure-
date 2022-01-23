5badc831f8f348409e1c041bbe888381c48538ff
Rajat_Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-01-23 21:34:28 +0530

    Update Clone Graph.cpp

--- Clone Graph.cpp
+++ Clone Graph.cpp
@@ -1,3 +1,24 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+    Node() {
+        val = 0;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val) {
+        val = _val;
+        neighbors = vector<Node*>();
+    }
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+
 class solution
 {
     void dfs(Node *current, Node *node, vector<Node *> &visited)
