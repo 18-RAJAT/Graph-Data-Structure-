e9fc24dc8654db0ffc028ffd2ca9d80fec59b6f3
Rajat_Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-01-23 21:11:14 +0530

    Add files via upload

---
+++ Clone Graph.cpp
@@ -0,0 +1,48 @@
+class solution
+{
+    void dfs(Node *current, Node *node, vector<Node *> &visited)
+    {
+        visited[node->val] = node;
+
+        for (auto element : current->neighbors)
+        {
+            if (visited[element->val] == NULL)
+            {
+                Node *newnode = new Node(element->val);
+                (node->neighbors).push_back(newnode);
+
+                dfs(element, newnode, visited);
+            }
+
+            else
+                (node->neighbors).push_back(visited[element->val]);
+        }
+    }
+
+public:
+    Node *cloneGraph(Node *node)
+    {
+        if (node == NULL)
+            return NULL;
+
+        vector<Node *> visited(1000, NULL);
+        Node *copy = new Node(node->val);
+
+        visited[node->val] = copy;
+
+        // Iterate for all neighbors
+
+        for (auto current : node->neighbors)
+        {
+            if (visited[current->val] == NULL)
+            {
+                Node *newnode = new Node(current->val);
+                (copy->neighbors).push_back(newnode);
+                dfs(current, newnode, visited);
+            }
+            else
+                (copy->neighbors).push_back(visited[current->val]);
+        }
+        return copy;
+    }
+};
